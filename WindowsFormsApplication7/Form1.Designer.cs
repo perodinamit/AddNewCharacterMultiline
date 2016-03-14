15fcfde08e3939f42734aded404062e6426875ac
perodinamit <matej.curcic@gmail.com>
2016-03-14 10:12:10 +0100

    Stavljeno uklanjanje backspacea i Copy/Paste funkcija

--- WindowsFormsApplication7/Form1.Designer.cs
+++ WindowsFormsApplication7/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -50,11 +53,44 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ukloni";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(347, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Copy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(347, 227);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Paste";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(454, 270);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -68,6 +104,9 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WindowsFormsApplication7/Form1.cs
+++ WindowsFormsApplication7/Form1.cs
@@ -30,9 +30,39 @@ namespace WindowsFormsApplication7
             lines[lines.Length - 1] = lines[lines.Length - 1].Substring(0, lines[lines.Length - 1].Length - 1);
 
             textBox1.Lines = lines;
-            string noviSring = lines[lines.Length - 1].Substring(0, lines[lines.Length - 1].Length-1);
+            
+        }
 
-            MessageBox.Show(noviSring);
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] lines = textBox1.Lines;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Substring(0, lines[i].Length - 1);
+            }
+            textBox1.Lines = lines;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.SelectionLength > 0)
+            {
+                textBox1.Copy();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.GetDataObject().GetDataPresent(DataFormats.Text)==true)
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to paste over current selection?","Cut", MessageBoxButtons.YesNo);
+                textBox1.Text = "";
+                if (dialogResult == DialogResult.Yes)
+                    {
+                        textBox1.SelectionStart = textBox1.SelectionStart + textBox1.SelectionLength;
+                    }
+                    textBox1.Paste();
+            }
         }
     }
 }
